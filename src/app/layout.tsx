28108348676d52f25befc3300074d7ebece47929
jorgeguedess <jorge_guedes00@hotmail.com>
2024-09-24 09:21:24 -0300

    chore: added standard Toast and font files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,28 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import "./globals.css";
 
-const inter = Inter({
-  variable: "--font-inter",
-  weight: ["400", "500", "600", "700"],
-  display: "swap",
-  subsets: ["latin"],
-  style: ["normal"]
-})
+import { inter } from "@/functions/fonts";
+import type { Metadata } from "next";
+
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
-  title: "Gestor de Pedidos - Telú Personalizados",
-  description: "Gestor de Pedidos - Telú Personalizados"
-}
+  title: "Telú Personalizados",
+  description: "Telú Personalizados"
+};
 
 export default function RootLayout({
   children
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${inter.variable} ${inter.className}`}>
-        <div className="flex min-h-screen flex-col">{children}</div>
+        <div className="flex min-h-screen flex-col">
+          <Toaster position="top-center" />
+          {children}
+        </div>
       </body>
     </html>
-  )
+  );
 }
