a78fd52c11a81396c4e902e39015172aa4aa93f5
jorgeguedess <jorge_guedes00@hotmail.com>
2024-09-24 20:32:37 -0300

    styles: modified Button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,15 +15,16 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline: "border border-input bg-background shadow-sm hover:bg-accent",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "border border-border bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline"
+        link: "text-inherit"
       },
       size: {
-        default: "h-11 px-4 py-2",
+        default: "h-10 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9"
+        icon: "h-9 w-9",
+        normal: "h-fit"
       }
     },
     defaultVariants: {
