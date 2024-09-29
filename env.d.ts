46f4753bb5c5030ce574b1fd191c98c4c2508489
jorgeguedess <jorge_guedes00@hotmail.com>
2024-09-29 14:02:31 -0300

    chore: add cloudinary and environment variables

--- env.d.ts
+++ env.d.ts
@@ -2,5 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     API_URL: string;
     JWT_SECRET: string;
+    CLOUD_NAME: string;
+    CLOUD_API_KEY: string;
+    CLOUD_API_SECRET: string;
   }
 }
