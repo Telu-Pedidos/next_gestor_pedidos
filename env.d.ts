908291bd8ddfb940a581301e9845e12baa4f09e3
jorgeguedess <jorge_guedes00@hotmail.com>
2024-10-02 22:35:25 -0300

    fix: adjusted environment variable names

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     API_URL: string;
     JWT_SECRET: string;
-    CLOUD_NAME: string;
-    CLOUD_API_KEY: string;
-    CLOUD_API_SECRET: string;
+    NEXT_PUBLIC_CLOUDINARY_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
   }
 }
